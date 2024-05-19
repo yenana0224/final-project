3cec5e91cb01d722936ce40315973b0ef1ca5104
nayeju <162558210+nayeju@users.noreply.github.com>
2024-05-19 22:55:22 +0900

    수정중

--- hondimoyeong/src/main/java/com/kh/hondimoyeong/course/controller/CourseController.java
+++ hondimoyeong/src/main/java/com/kh/hondimoyeong/course/controller/CourseController.java
@@ -3,6 +3,7 @@ package com.kh.hondimoyeong.course.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.hondimoyeong.course.model.service.CourseService;
@@ -13,12 +14,16 @@ public class CourseController {
 	@Autowired
 	private CourseService courseService;
 	
-	@GetMapping("course")
+	@GetMapping("courses")
 	public ModelAndView CourseMain(ModelAndView mv) {
-		
 		mv.addObject("list", courseService.allCourseList()).setViewName("course/courseMain");
-		
 		return mv;
+	}
+	
+	@GetMapping("course/{courseIndex}")
+	public String courseDetail(@PathVariable int courseIndex) {
+		System.out.println(courseIndex);
+		return "";
 	}
 
 }
