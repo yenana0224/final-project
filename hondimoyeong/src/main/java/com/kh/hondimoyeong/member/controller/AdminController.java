25e8d07129e91d84cda87613ca3f8fe6dc77bad7
SeoJunHyeong <110711429+JunHyeongSeo@users.noreply.github.com>
2024-05-21 15:37:36 +0900

    12

--- hondimoyeong/src/main/java/com/kh/hondimoyeong/member/controller/AdminController.java
+++ hondimoyeong/src/main/java/com/kh/hondimoyeong/member/controller/AdminController.java
@@ -1,10 +1,11 @@
 package com.kh.hondimoyeong.member.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,6 +48,9 @@ public class AdminController {
 
 		PageInfo pi = Pagination.getPageInfo(reserveService.selectListCount(), page, 5, 5);
 		System.out.println(pi);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("list", reserveService.selectList(pi));
 		
 		return new Gson().toJson(pi);
 	}
